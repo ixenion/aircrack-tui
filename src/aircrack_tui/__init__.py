a85de7ad7d1bf6b7b75353faded87e03e6163185
ixenion <stridjj@gmail.com>
2024-12-13 12:26:45 +0300

    Initial commit.

---
+++ src/aircrack_tui/__init__.py
@@ -0,0 +1,6 @@
+__app_name__ = "aicrack-tui"
+__version__ = "0.1.0"
+
+# from gnss_service.gnss       import GNSS
+# from gnss_service.utils.datastructures   import NMEA
+from aircrack_tui.main      import app
---
+++ src/aircrack_tui/main.py
@@ -0,0 +1,106 @@
+# ----------------#
+# System imports #
+# ----------------#
+
+import asyncio
+from dataclasses            import asdict
+from os                     import geteuid
+from pprint                 import pprint
+from time                   import time, gmtime, strftime
+from typing                 import AsyncGenerator, Optional
+
+
+# ------------------- #
+# Third party imports #
+
+import typer
+
+
+# -------------#
+# Local imports
+# -------------#
+
+from aircrack_tui import __app_name__, __version__
+
+
+# --------- #
+# FUNCTIONS #
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit()
+
+
+# ------- #
+# CLASSES #
+
+
+
+
+# ------ #
+#  MAIN  #
+# ------ #
+
+async def main() -> None:
+    """
+    """
+    
+    ...
+
+
+
+
+
+#################
+# BUILD CLI APP #
+#################
+
+# 'add_completion=False' - get rid of
+# --install-completion, --show-completion
+# app = typer.Typer(add_completion=False)
+app = typer.Typer()
+
+@app.command()
+def control(
+        version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version and exit.",
+        callback=_version_callback,
+        is_eager=True),
+
+        # mode:Keys.MODE = typer.Option(
+        #     default=Keys.MODE.real,
+        #     help="Set NMEA source. Real data only with root."),
+        # consumer:Keys.CONSUMER = typer.Option(
+        #     default=Keys.CONSUMER.stdout,
+        #     help="Set data consumer."),
+        # delay:float = typer.Option(
+        #     default=0.2,
+        #     help="Set GNSS position fix in sec.",
+        #     min=0.2,
+        #     max=10,
+        #     ),
+
+        ):
+
+    """
+    """
+
+    # asyncio.run(main(mode, consumer, delay))
+    asyncio.run(main())
+
+
+def app_entry_point():
+    """
+        To run dirrectly from console (uses pdm build engine).
+        Example:
+    """
+
+    app()
+
+
+if __name__ == "__main__":
+    app()
